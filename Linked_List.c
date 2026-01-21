4fe317b8a553e452d6cc1320ab29efeb6dbecbf2
Jayanesh <72813129+jayanesh@users.noreply.github.com>
2026-01-21 11:27:07 +0530

    Does not work
    I'm pretty dumb

--- Linked_List.c
+++ Linked_List.c
@@ -1,46 +1,17 @@
 #include<stdio.h>
-
-//structure For Linked List.
-
+#include<stdlib.h>
 struct node{
     int data;
-
     struct node* next;//self referencial
+};
+
+
+struct node* create(int d){
+    struct node* nn;
+    nn=(struct node*)malloc(sizeof(struct node));
+    nn->data=d;
+    nn->next=NULL;
 }
-
-
-int main(){
-    struct node* head=NULL;
-    int ch;
-    while(1){
-        printf("Linked List Operations:\n1. Insertion\n2 .Count\nEnter Your choice:");
-        scanf("%d",&ch);
-        switch(ch){
-            case 1:
-                int pos_ch,data;
-                printf("Insertion type:\n1. Start\n2. End\n3. Positioned\nEnter Your Choice:");
-                scanf("%d",&pos_ch);
-                printf("\nEnter Data to store:");
-                scanf("%d",&data);
-                switch(pos_ch){
-                    case 1:
-                        insert_beg(&head,data);
-                    case 2:
-                        insert_end(&head,data);
-                    case 3:
-                        insert_pos(&head);
-                }
-                break;
-            case 2:
-                printf("The count Linked List is: %d",count(&head));
-                break;
-            //case 3:
-                
-                
-        }
-    }
-}
-
 void insert_beg(struct node** q,int d){
     struct node* nn;
     nn=create(d);
@@ -48,5 +19,43 @@ void insert_beg(struct node** q,int d){
     else{
         nn->next=*q;
         *q=nn;
+    }
+}
+void displayll(struct node* q){
+    struct node* temp;
+    temp=q;
+    while(temp->next!=NULL){
+        printf("%d ",temp->data);
+        temp=temp->next;
+    }
+}
+
+void delete_at_beg(struct node** q){
+    struct node* temp;
+    temp=*q;
+    if(temp==NULL) printf("Underflow");
+    else{
+        *q=temp->next;
+        free(temp);
+    }
+}
+int main(){
+    struct node* head=NULL;
+    int data;
+    int ch;
+    while(1){
+        printf("Enter choice:\n1. insert\n2. delete\n3. Exit\nChoose:");
+        scanf("%d",&ch);
+        if(ch==1){
+            printf("Enter value to store:");
+            scanf("%d",&data);
+            insert_beg(&head,data);
+            displayll(head);
+        }
+        if(ch==2){
+            delete_at_beg(&head);
+            displayll(head);
+        }
+        if(ch==3) return 0;
     }
 }
